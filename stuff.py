27eb0f8907dc9b8d4e235e08a089edc19fcc2291
Ben-Littell <89919066+Ben-Littell@users.noreply.github.com>
2021-10-25 09:39:09 -0400

    guk

--- stuff.py
+++ stuff.py
@@ -1,30 +1,44 @@
 # Look to the slideshow in google classroom for notes
 import csv
 
+prompt = input('Enter a query: ')
 
-# prompt = input('Enter a query: ')
 
+########################################################################################################################
+#                                            Errors
+# def open_file():
+#     company_db_dict = {}
+#     list1 = []
+#     list2 = []
+#     file_row_numb = 0
+#     with open('company_db.csv') as db:
+#         company_db = csv.reader(db)
+#         line1 = db.readline()
+#         lines = db.readlines()[1:]
+#         keys = company_db[0]
+#         line1_len = len(line1.split(','))
+#         for i in line1.split(','):
+#             company_db_dict[i.strip()] = []
+#             for row in lines:
+#                 company_db_dict[keys[i]].append(row[i])
+
+#         for item in company_db:
+#             list1.append(item[value])
+#             company_db_dict.update({line1.split(',')[6]: list1[0:len(list1)-1]})
+#
+# return company_db_dict
+########################################################################################################################
 
 def open_file():
     company_db_dict = {}
-    list1 = []
-    list2 = []
-    file_row_numb = 0
     with open('company_db.csv') as db:
-        company_db = csv.reader(db)
-        line1 = db.readline()
-        lines = db.readlines()[1:]
-        print(lines)
-        line1_len = len(line1.split(','))
-        for i in line1.split(','):
-            company_db_dict[i.strip()] = []
-            for line in lines:
-
-    #         for item in company_db:
-    #             list1.append(item[value])
-    #             company_db_dict.update({line1.split(',')[6]: list1[0:len(list1)-1]})
-    #
-    # return company_db_dict
+        company_db = list(csv.reader(db))
+        keys = company_db[0]
+        for i in range(len(keys)):
+            company_db_dict[keys[i]] = []
+            for row in company_db[1:]:
+                company_db_dict[keys[i]].append(row[i])
+        return company_db_dict
 
 
 def check_tokens(tokens, csv_dict):
@@ -53,4 +67,4 @@ def check_tokens(tokens, csv_dict):
 file = open_file()
 # print(file)
 
-# check_tokens(prompt, file)
+check_tokens(prompt, file)
