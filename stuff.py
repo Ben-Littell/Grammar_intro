ce720886413b1a3626fa9af10da8e820a0a74ae0
Ben-Littell <89919066+Ben-Littell@users.noreply.github.com>
2021-10-29 09:10:43 -0400

    new open file

--- stuff.py
+++ stuff.py
@@ -1,44 +1,24 @@
 # Look to the slideshow in google classroom for notes
 import csv
 
-prompt = input('Enter a query: ')
+# prompt = input('Enter a query: ')
 
 
 ########################################################################################################################
-#                                            Errors
-# def open_file():
-#     company_db_dict = {}
-#     list1 = []
-#     list2 = []
-#     file_row_numb = 0
-#     with open('company_db.csv') as db:
-#         company_db = csv.reader(db)
-#         line1 = db.readline()
-#         lines = db.readlines()[1:]
-#         keys = company_db[0]
-#         line1_len = len(line1.split(','))
-#         for i in line1.split(','):
-#             company_db_dict[i.strip()] = []
-#             for row in lines:
-#                 company_db_dict[keys[i]].append(row[i])
 
-#         for item in company_db:
-#             list1.append(item[value])
-#             company_db_dict.update({line1.split(',')[6]: list1[0:len(list1)-1]})
-#
-# return company_db_dict
 ########################################################################################################################
 
 def open_file():
-    company_db_dict = {}
+    company_db_list = []
     with open('company_db.csv') as db:
         company_db = list(csv.reader(db))
         keys = company_db[0]
-        for i in range(len(keys)):
-            company_db_dict[keys[i]] = []
-            for row in company_db[1:]:
-                company_db_dict[keys[i]].append(row[i])
-        return company_db_dict
+        for i in company_db[1:]:
+            empty_dict = {}
+            for j in range(len(keys)):
+                empty_dict[keys[j]] = i[j]
+            company_db_list.append(empty_dict)
+    return company_db_list
 
 
 def check_tokens(tokens, csv_dict):
@@ -69,7 +49,7 @@ def check_tokens(tokens, csv_dict):
 
 
 file = open_file()
-# print(file)
+print(file)
 
-tokens_g = check_tokens(prompt, file)
+# tokens_g = check_tokens(prompt, file)
 
